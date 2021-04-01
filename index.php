faaf2a76f46e385780dbdf3dd31312deda745958
yenlinh000 <yenlinh.nc21@gmail.com>
2021-04-01 16:12:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,53 @@
-<!DOCTYPE html> 
-<html> 
-<head> 
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
-<link rel="stylesheet" href="style.css"/> 
-</head> 
-<body> 
-<form action='login.php' class="dangnhap" method='POST'> 
-Tên đăng nhập : <input type='text' name='username' /> 
-Mật khẩu : <input type='password' name='password' /> 
-<input type='submit' class="button" name="dangnhap" value='Đăng nhập' /> 
-<a href='dangky.php' title='Đăng ký'>Đăng ký</a> 
-<?php include 'conn.php';?>
-</form> 
-</body> 
+$host = "ec2-54-235-108-217.compute-1.amazonaws.com";
+$port = "5432";
+$dbname = "dav987822a4d5g";
+$user = "jkbkabjuswknzz";
+$password = "433742a75814552fccc3d49c54551698f4f73311e15261248003c93a7fda66f0"; 
+$connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
+$dbconn = pg_connect($connection_string);
+if(isset($_POST['submit'])&&!empty($_POST['submit'])){
+    
+    $hashpassword = md5($_POST['pwd']);
+    $sql ="select *from public.tbl_user where username = '".pg_escape_string($_POST['username'])."' and password ='".$hashpassword."'";
+    $data = pg_query($dbconn,$sql); 
+    $login_check = pg_num_rows($data);
+    if($login_check > 0){ 
+        
+        echo "Login Successfully";    
+    }else{
+        
+        echo "Invalid Details";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>PHP PostgreSQL Registration & Login Example </title>
+  <meta name="keywords" content="PHP,PostgreSQL,Insert,Login">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+  <h2>Login Here </h2>
+  <form method="post">
+  
+     
+    <div class="form-group">
+      <label for="username">Username:</label>
+      <input type="username" class="form-control" id="username" placeholder="Enter username" name="username">
+    </div>
+    
+     
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+    </div>
+     
+    <input type="submit" name="submit" class="btn btn-primary" value="Submit">
+  </form>
+</div>
+</body>
 </html>
